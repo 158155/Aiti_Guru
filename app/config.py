db58089b3a41178875c645284fd60ffb452413f1
158155 <158155@gmail.com>
2026-04-03 17:44:59 +0300

    add: Начальная структура+конфиг+настройка логгирования

---
+++ app/config.py
@@ -0,0 +1,26 @@
+import logging
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env"
+        )
+    
+    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/aiti_guru_test"
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8000
+
+    LOG_LEVEL: str = "INFO"
+    LOG_FORMAT: str = "%(asctime)s - %(levelname)s - %(message)s"
+    
+settings = Settings()
+
+
+def setup_logging() -> None:
+    """Настройка корневого лдоггера из конфига"""
+    logging.basicConfig(
+        level=getattr(logging, settings.LOG_LEVEL.upper(), logging.INFO),
+        format=settings.LOG_FORMAT,
+    )
+    
